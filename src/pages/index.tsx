753c4828247a41ed091969019073e491d97c43c1
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-07-14 08:29:53 +0900

    ♻️ リファクタリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,6 @@ type Props = {
   };
 };
 
-type PreObj = {
-  id: number;
-  prefecture: string;
-};
-
 // SGで都道府県一覧を取得
 export const getStaticProps = async () => {
   const URL: string =
@@ -30,22 +25,16 @@ export const getStaticProps = async () => {
 const Home: NextPage<Props> = (props: Props) => {
   const prefectures: string[] = props.prefectures.response.prefecture;
 
-  // prefecturesをkeyがあるオブジェクトに格納する
-  let preObjArray: PreObj[] = [];
-  for (let i = 0; i < prefectures.length; i++) {
-    const preObj: PreObj = {
-      id: i,
-      prefecture: prefectures[i],
-    };
-    preObjArray.push(preObj);
-  }
-
   return (
     <div>
       <span>都道府県: </span>
       <select name="prefecture">
-        {preObjArray.map((preObj) => {
-          return <option key={preObj.id}>{preObj.prefecture}</option>;
+        {prefectures.map((prefecture, i) => {
+          return (
+            <option key={i} value={prefecture}>
+              {prefecture}
+            </option>
+          );
         })}
       </select>
     </div>
